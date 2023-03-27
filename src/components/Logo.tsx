6c93bde30f8fbcba1c300dc07bdcf0c91c1423fc
Ahmed Ghonem <aghonem2011@gmail.com>
2023-03-27 22:27:28 +0200

    style: 💄 replaced the logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,22 @@
 import { Image, ImageProps } from '@chakra-ui/react';
 import React from 'react';
-import logo from '../assets/logo.svg';
+import logo from '../assets/wide-logo.png';
 
 interface LogoProps {
   logoProps?: ImageProps;
 }
 
 const Logo: React.FC<LogoProps> = ({ logoProps }) => {
-  return <Image src={logo} alt='LeetSync' {...logoProps} />;
+  return (
+    <Image
+      src={logo}
+      alt='LeetSync'
+      {...logoProps}
+      maxW='160px'
+      borderRadius={'50%'}
+      boxShadow={'dark-lg'}
+      mb={'0.5rem'}
+    />
+  );
 };
 export default Logo;
--- src/modules/CompleteAuthentication.tsx
+++ src/modules/CompleteAuthentication.tsx
@@ -212,8 +212,8 @@ const StartOnboarding = ({ nextStep }: { nextStep: Function }) => {
       <VStack w='100%'>
         <Heading size='lg'>Welcome 👋</Heading>
         <Text color='GrayText' fontSize={'sm'} w='90%' textAlign={'center'}>
-          LeetSync is a Chrome extension that syncs your LeetCode solutions to
-          GitHub. Setup now.
+          LeetSync is a Chrome extension that syncs your submissions to GitHub.
+          Setup now.
         </Text>
       </VStack>
 
