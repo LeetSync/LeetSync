f0a3fb6935243ec244a4e4a14857218338a336c7
Ahmed Ghonem <aghonem2011@gmail.com>
2023-04-04 04:13:48 +0200

    chore:  added a config to disable emojis when working with cz

---
+++ changelog.config.js
@@ -0,0 +1,79 @@
+module.exports = {
+  breakingChangePrefix: '',
+  closedIssuePrefix: '',
+  list: [
+    'test',
+    'feat',
+    'fix',
+    'chore',
+    'docs',
+    'refactor',
+    'style',
+    'ci',
+    'perf',
+  ],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  questions: [
+    'type',
+    'scope',
+    'subject',
+    'body',
+    'breaking',
+    'issues',
+    'lerna',
+  ],
+  scopes: [],
+  types: {
+    chore: {
+      description: 'Build process or auxiliary tool changes',
+      emoji: '',
+      value: 'chore',
+    },
+    ci: {
+      description: 'CI related changes',
+      emoji: '',
+      value: 'ci',
+    },
+    docs: {
+      description: 'Documentation only changes',
+      emoji: '',
+      value: 'docs',
+    },
+    feat: {
+      description: 'A new feature',
+      emoji: '',
+      value: 'feat',
+    },
+    fix: {
+      description: 'A bug fix',
+      emoji: '',
+      value: 'fix',
+    },
+    perf: {
+      description: 'A code change that improves performance',
+      emoji: '',
+      value: 'perf',
+    },
+    refactor: {
+      description: 'A code change that neither fixes a bug or adds a feature',
+      emoji: '',
+      value: 'refactor',
+    },
+    release: {
+      description: 'Create a release commit',
+      emoji: '',
+      value: 'release',
+    },
+    style: {
+      description: 'Markup, white-space, formatting, missing semi-colons...',
+      emoji: '',
+      value: 'style',
+    },
+    test: {
+      description: 'Adding missing tests',
+      emoji: '',
+      value: 'test',
+    },
+  },
+};
