228c5ee06cee38931728b0ebe6d5336265a800ae
Ahmed Ghonem <aghonem2011@gmail.com>
2024-02-10 19:18:14 +0200

    Refactor LeetCodeHandler and remove unused import

--- src/handlers/LeetCodeHandler.ts
+++ src/handlers/LeetCodeHandler.ts
@@ -1,13 +1,9 @@
-import {
-  getAllSubmission,
-  getSubmission,
-} from '../api/submissions/getSubmission';
+import { getAllSubmission, getSubmission } from '../api/submissions/getSubmission';
 import { Submission } from '../types/Submission';
+
 class LeetCodeHandler {
   async getSubmission(questionSlug: string): Promise<Submission | null> {
-    const leetcode_session = (
-      await chrome.storage.sync.get('leetcode_session')
-    )?.['leetcode_session'];
+    const leetcode_session = (await chrome.storage.sync.get('leetcode_session'))?.['leetcode_session'];
 
     if (!leetcode_session) {
       return null;
@@ -20,8 +16,7 @@ class LeetCodeHandler {
       return null;
     }
 
-    const latestSubmissionId =
-      submissions?.questionSubmissionList?.submissions?.[0].id;
+    const latestSubmissionId = submissions?.questionSubmissionList?.submissions?.[0].id;
 
     const result = await getSubmission(latestSubmissionId, leetcode_session);
 
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,5 @@
 //an entry point for all the classes
 import GithubHandler from './GithubHandler';
 import LeetCodeHandler from './LeetCodeHandler';
-import QuestionHandler from './QuestionHandler';
 
-export { GithubHandler, LeetCodeHandler, QuestionHandler };
+export { GithubHandler, LeetCodeHandler };
