d05dce68764cc17484588f5a65560750b221f91a
Ahmed Ghonem <aghonem2011@gmail.com>
2024-02-10 19:03:41 +0200

    Added retry policy for getting the submissions

--- src/scripts/leetcode.ts
+++ src/scripts/leetcode.ts
@@ -5,19 +5,21 @@ import { LeetCodeHandler, GithubHandler } from '../handlers';
 const leetcode = new LeetCodeHandler();
 const github = new GithubHandler();
 
-chrome.runtime.onMessage.addListener(async function (
-  request,
-  s,
-  _sendResponse
-) {
+const sleep = async (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+chrome.runtime.onMessage.addListener(async function (request, _s, _sendResponse) {
   if (request && request.type === 'get-submission') {
     const questionSlug = request?.data?.questionSlug;
 
     if (!questionSlug) return;
 
-    const submission = await leetcode.getSubmission(questionSlug);
+    let retries = 0;
+    let submission = await leetcode.getSubmission(questionSlug);
+    while (!submission && retries < 3) {
+      retries++;
+      await sleep(retries * 1000);
+      submission = await leetcode.getSubmission(questionSlug);
+    }
     if (!submission) return;
-
     //validate submission's timestamp, if its was submitted more than 1 minute ago, then its an old submission and we should ignore it
     const now = new Date();
     const submissionDate = new Date(submission.timestamp * 1000);
